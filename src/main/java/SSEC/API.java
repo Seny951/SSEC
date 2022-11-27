c894a9396e8074e0c4202dc4fcd4f4037384c24b
Seny951 <seny951@hotmail.com>
2022-11-27 09:19:33 +0000

    added authentication consideration

--- src/main/java/SSEC/API.java
+++ src/main/java/SSEC/API.java
@@ -10,6 +10,23 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/* Authentication:
+* For Authentication I would opt for a TLS approach.
+*   Client Certificates could be generated using user credentials or a license key linked with a user account and
+* valid serial / device ids in order to ensure the certificate is unique to a user account or specific device
+* and cannot be transferred to be used elsewhere.
+*
+*   A client would initially request a key via an unsecure connection (i.e. an http interface),
+* providing information about the user account and/or device
+* which would be used by the server to generate a certificate unique to that user or device.
+*
+*   Once a certificate is obtained further api requests will be made on a secure connection (i.e. HTTPS) that
+* validates certificates on each request via a TLS handshake and certification transfer.
+*
+*   An added benefit of TLS is that as well as the caller being validated the data transfer is also encrypted
+ */
+
+
 public class API {
     static ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
     static Logger logger = Logger.getLogger(API.class.getName());
